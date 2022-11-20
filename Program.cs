4ac049ab6d3bf96f0679dcb7c451fb0301b2c277
Serafima88213 <115182050+Serafima88213@users.noreply.github.com>
2022-11-20 16:25:42 +0300

    DZ

---
+++ Program.cs
@@ -0,0 +1,52 @@
+ /*
+//  Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+Console.WriteLine("The equation is y = kx +b");
+
+Console.WriteLine("Enter k1");
+double k1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Enter b1");
+double b1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Enter k2");
+double k2 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Enter b2");
+double b2 = Convert.ToDouble(Console.ReadLine());
+
+if(k1=k2)
+{
+   Console.WriteLine("The lines are parallel"); 
+}
+else
+{
+    double x = (b2 - b1)/(k1 -k2);
+    double y = k1*x + b1;
+
+    Console.WriteLine($"The solvation is:({x};{y})");
+}
+*/
+
+/*
+
+// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+// 0, 7, 8, -2, -2 -> 2
+
+// 1, -7, 567, 89, 223-> 3
+
+Console.WriteLine("Amont of numbers :");
+int n = Convert.ToInt32(Console.Readline());
+int sum =0;
+int[] a = new int[n];
+for(int i = 0; i < n; i++)
+{
+ Console.Write($"A[{i + 1}] = ");  
+ a[i] = Convert.ToInt32(Console.ReadLine());
+ if (a[i] > 0)
+ {
+    sum++;
+ } 
+}
+Console.WriteLine("Amont of positive numbers:{sum}");
+*/
